ed3a6fbd02edf07619cf8048cc4c4436628b88d0
Riza Khan <riza.khan@appnovation.com>
2021-08-30 00:10:57 -0400

    ability to see the db

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const axios = require("axios");
+const mysql = require("mysql2");
 
-const getInfo = async () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/posts/1")
-    .then((res) => console.log(res.data))
-    .catch((e) => console.log(e));
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "terminal",
+});
+
+const getAllData = () => {
+  console.log("con", con);
+  con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM git", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
 };
 
-exports.getInfo = getInfo;
+exports.getAllData = getAllData;
