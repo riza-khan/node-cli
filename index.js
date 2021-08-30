2e085bd924c384052cc34c2ab866525afb5e74c5
Riza Khan <riza.khan@appnovation.com>
2021-08-30 00:31:26 -0400

    able to query

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const con = mysql.createConnection({
   database: "terminal",
 });
 
-const getAllData = () => {
-  console.log("con", con);
-  con.connect(function (err) {
-    if (err) throw err;
-    con.query("SELECT * FROM git", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  });
-};
+class DB {
+  constructor(con) {
+    this.con = con;
+  }
 
-exports.getAllData = getAllData;
+  getAllData() {
+    this.con.connect((err) => {
+      if (err) throw err;
+      this.con.query("SELECT * FROM git", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
+    });
+  }
+}
+
+const db = new DB(con);
+
+exports.db = db;
