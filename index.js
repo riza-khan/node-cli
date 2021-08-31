8bd86af99db40aa49c70009d35d5ac1552a7d3a4
Riza Khan <riza.khan@appnovation.com>
2021-08-30 21:20:26 -0400

    Branch: git-hooks
    Jira: undefined
    Type: undefined

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ class DB {
         .then((result) => {
           const jira_link = result[0].jira_link;
           const type = result[0].type;
-          console.log(jira_link, type);
+          console.log({ jira_link, type });
           process.exit(0);
         })
         .catch((e) => {
